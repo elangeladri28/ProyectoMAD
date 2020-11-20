0e63723c9383ca23882d3ab3dd904bb29da9b72d
Adrian Martinez <carnallostes@gmail.com>
2020-11-20 00:59:37 -0600

    Avances de puta madre

--- ProjectMAD/ProjectMAD/Forms/Nuevo Departamento.Designer.vb
+++ ProjectMAD/ProjectMAD/Forms/Nuevo Departamento.Designer.vb
@@ -28,16 +28,19 @@ Partial Class FormNuevoDepartamento
         Me.LabelNuevoDepartamento_Gerente = New System.Windows.Forms.Label()
         Me.TextBoxNuevoDepartamento_Gerente = New System.Windows.Forms.TextBox()
         Me.LabelNuevoDepartamento_SueldoBase = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBoxNuevoDepartamento_Sueldo = New System.Windows.Forms.TextBox()
+        Me.ButtonNuevoDepartamento_Cancelar = New System.Windows.Forms.Button()
+        Me.ButtonNuevoDepartamento_Guardar = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'LabelNuevoDepartamento_Instruccion
         '
         Me.LabelNuevoDepartamento_Instruccion.AutoSize = True
         Me.LabelNuevoDepartamento_Instruccion.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.LabelNuevoDepartamento_Instruccion.Location = New System.Drawing.Point(36, 9)
+        Me.LabelNuevoDepartamento_Instruccion.Location = New System.Drawing.Point(48, 11)
+        Me.LabelNuevoDepartamento_Instruccion.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LabelNuevoDepartamento_Instruccion.Name = "LabelNuevoDepartamento_Instruccion"
-        Me.LabelNuevoDepartamento_Instruccion.Size = New System.Drawing.Size(343, 32)
+        Me.LabelNuevoDepartamento_Instruccion.Size = New System.Drawing.Size(433, 40)
         Me.LabelNuevoDepartamento_Instruccion.TabIndex = 0
         Me.LabelNuevoDepartamento_Instruccion.Text = "LLENAR LOS SIGUIENTES CAMPOS PARA AGREGAR" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & " UN NUEVO DEPARTAMENTO AL SISTEMA" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
         Me.LabelNuevoDepartamento_Instruccion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
@@ -45,64 +48,91 @@ Partial Class FormNuevoDepartamento
         'LabelNuevoDepartamento_Nombre
         '
         Me.LabelNuevoDepartamento_Nombre.AutoSize = True
-        Me.LabelNuevoDepartamento_Nombre.Location = New System.Drawing.Point(12, 74)
+        Me.LabelNuevoDepartamento_Nombre.Location = New System.Drawing.Point(166, 60)
+        Me.LabelNuevoDepartamento_Nombre.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LabelNuevoDepartamento_Nombre.Name = "LabelNuevoDepartamento_Nombre"
-        Me.LabelNuevoDepartamento_Nombre.Size = New System.Drawing.Size(47, 13)
+        Me.LabelNuevoDepartamento_Nombre.Size = New System.Drawing.Size(62, 17)
         Me.LabelNuevoDepartamento_Nombre.TabIndex = 1
         Me.LabelNuevoDepartamento_Nombre.Text = "Nombre:"
         '
         'TextBoxNuevoDepartamento_Nombre
         '
-        Me.TextBoxNuevoDepartamento_Nombre.Location = New System.Drawing.Point(15, 90)
+        Me.TextBoxNuevoDepartamento_Nombre.Location = New System.Drawing.Point(170, 80)
+        Me.TextBoxNuevoDepartamento_Nombre.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBoxNuevoDepartamento_Nombre.Name = "TextBoxNuevoDepartamento_Nombre"
-        Me.TextBoxNuevoDepartamento_Nombre.Size = New System.Drawing.Size(148, 20)
+        Me.TextBoxNuevoDepartamento_Nombre.Size = New System.Drawing.Size(196, 22)
         Me.TextBoxNuevoDepartamento_Nombre.TabIndex = 2
         '
         'LabelNuevoDepartamento_Gerente
         '
         Me.LabelNuevoDepartamento_Gerente.AutoSize = True
-        Me.LabelNuevoDepartamento_Gerente.Location = New System.Drawing.Point(12, 130)
+        Me.LabelNuevoDepartamento_Gerente.Location = New System.Drawing.Point(166, 129)
+        Me.LabelNuevoDepartamento_Gerente.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LabelNuevoDepartamento_Gerente.Name = "LabelNuevoDepartamento_Gerente"
-        Me.LabelNuevoDepartamento_Gerente.Size = New System.Drawing.Size(48, 13)
+        Me.LabelNuevoDepartamento_Gerente.Size = New System.Drawing.Size(64, 17)
         Me.LabelNuevoDepartamento_Gerente.TabIndex = 3
         Me.LabelNuevoDepartamento_Gerente.Text = "Gerente:"
         '
         'TextBoxNuevoDepartamento_Gerente
         '
-        Me.TextBoxNuevoDepartamento_Gerente.Location = New System.Drawing.Point(15, 147)
+        Me.TextBoxNuevoDepartamento_Gerente.Location = New System.Drawing.Point(170, 150)
+        Me.TextBoxNuevoDepartamento_Gerente.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.TextBoxNuevoDepartamento_Gerente.Name = "TextBoxNuevoDepartamento_Gerente"
-        Me.TextBoxNuevoDepartamento_Gerente.Size = New System.Drawing.Size(148, 20)
+        Me.TextBoxNuevoDepartamento_Gerente.Size = New System.Drawing.Size(196, 22)
         Me.TextBoxNuevoDepartamento_Gerente.TabIndex = 4
         '
         'LabelNuevoDepartamento_SueldoBase
         '
         Me.LabelNuevoDepartamento_SueldoBase.AutoSize = True
-        Me.LabelNuevoDepartamento_SueldoBase.Location = New System.Drawing.Point(15, 193)
+        Me.LabelNuevoDepartamento_SueldoBase.Location = New System.Drawing.Point(166, 205)
+        Me.LabelNuevoDepartamento_SueldoBase.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.LabelNuevoDepartamento_SueldoBase.Name = "LabelNuevoDepartamento_SueldoBase"
-        Me.LabelNuevoDepartamento_SueldoBase.Size = New System.Drawing.Size(70, 13)
+        Me.LabelNuevoDepartamento_SueldoBase.Size = New System.Drawing.Size(92, 17)
         Me.LabelNuevoDepartamento_SueldoBase.TabIndex = 5
         Me.LabelNuevoDepartamento_SueldoBase.Text = "Sueldo Base:"
         '
-        'TextBox1
+        'TextBoxNuevoDepartamento_Sueldo
         '
-        Me.TextBox1.Location = New System.Drawing.Point(18, 209)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(145, 20)
-        Me.TextBox1.TabIndex = 6
+        Me.TextBoxNuevoDepartamento_Sueldo.Location = New System.Drawing.Point(169, 226)
+        Me.TextBoxNuevoDepartamento_Sueldo.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TextBoxNuevoDepartamento_Sueldo.Name = "TextBoxNuevoDepartamento_Sueldo"
+        Me.TextBoxNuevoDepartamento_Sueldo.Size = New System.Drawing.Size(197, 22)
+        Me.TextBoxNuevoDepartamento_Sueldo.TabIndex = 6
+        '
+        'ButtonNuevoDepartamento_Cancelar
+        '
+        Me.ButtonNuevoDepartamento_Cancelar.Location = New System.Drawing.Point(116, 287)
+        Me.ButtonNuevoDepartamento_Cancelar.Name = "ButtonNuevoDepartamento_Cancelar"
+        Me.ButtonNuevoDepartamento_Cancelar.Size = New System.Drawing.Size(99, 34)
+        Me.ButtonNuevoDepartamento_Cancelar.TabIndex = 7
+        Me.ButtonNuevoDepartamento_Cancelar.Text = "Cancelar"
+        Me.ButtonNuevoDepartamento_Cancelar.UseVisualStyleBackColor = True
+        '
+        'ButtonNuevoDepartamento_Guardar
+        '
+        Me.ButtonNuevoDepartamento_Guardar.Location = New System.Drawing.Point(320, 287)
+        Me.ButtonNuevoDepartamento_Guardar.Name = "ButtonNuevoDepartamento_Guardar"
+        Me.ButtonNuevoDepartamento_Guardar.Size = New System.Drawing.Size(99, 34)
+        Me.ButtonNuevoDepartamento_Guardar.TabIndex = 8
+        Me.ButtonNuevoDepartamento_Guardar.Text = "Guardar"
+        Me.ButtonNuevoDepartamento_Guardar.UseVisualStyleBackColor = True
         '
         'FormNuevoDepartamento
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.SteelBlue
-        Me.ClientSize = New System.Drawing.Size(427, 462)
-        Me.Controls.Add(Me.TextBox1)
+        Me.ClientSize = New System.Drawing.Size(534, 344)
+        Me.Controls.Add(Me.ButtonNuevoDepartamento_Guardar)
+        Me.Controls.Add(Me.ButtonNuevoDepartamento_Cancelar)
+        Me.Controls.Add(Me.TextBoxNuevoDepartamento_Sueldo)
         Me.Controls.Add(Me.LabelNuevoDepartamento_SueldoBase)
         Me.Controls.Add(Me.TextBoxNuevoDepartamento_Gerente)
         Me.Controls.Add(Me.LabelNuevoDepartamento_Gerente)
         Me.Controls.Add(Me.TextBoxNuevoDepartamento_Nombre)
         Me.Controls.Add(Me.LabelNuevoDepartamento_Nombre)
         Me.Controls.Add(Me.LabelNuevoDepartamento_Instruccion)
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "FormNuevoDepartamento"
         Me.Text = "Nuevo Departamento"
         Me.ResumeLayout(False)
@@ -116,5 +146,7 @@ Partial Class FormNuevoDepartamento
     Friend WithEvents LabelNuevoDepartamento_Gerente As Label
     Friend WithEvents TextBoxNuevoDepartamento_Gerente As TextBox
     Friend WithEvents LabelNuevoDepartamento_SueldoBase As Label
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBoxNuevoDepartamento_Sueldo As TextBox
+    Friend WithEvents ButtonNuevoDepartamento_Cancelar As Button
+    Friend WithEvents ButtonNuevoDepartamento_Guardar As Button
 End Class
--- ProjectMAD/ProjectMAD/Forms/Nuevo Departamento.vb
+++ ProjectMAD/ProjectMAD/Forms/Nuevo Departamento.vb
@@ -1,3 +1,41 @@
 ﻿Public Class FormNuevoDepartamento
+    Private Sub TextBoxNuevoDepartamento_Nombre_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBoxNuevoDepartamento_Nombre.KeyPress
+        If Char.IsLetter(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsControl(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsSeparator(e.KeyChar) Then
+            e.Handled = False
+        Else
+            e.Handled = True
+        End If
+    End Sub
 
+    Private Sub TextBoxNuevoDepartamento_Sueldo_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBoxNuevoDepartamento_Sueldo.KeyPress
+        If Char.IsNumber(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsControl(e.KeyChar) Then
+            e.Handled = False
+        ElseIf Char.IsSeparator(e.KeyChar) Then
+            e.Handled = False
+        Else
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub ButtonNuevoDepartamento_Cancelar_Click(sender As Object, e As EventArgs) Handles ButtonNuevoDepartamento_Cancelar.Click
+        Dim Resp
+        Resp = MsgBox("¿Estás seguro de que deseas salir?, ningún dato sera guardado", vbYesNo, "Advertencia")
+        If Resp = vbYes Then
+            Me.Close()
+        End If
+    End Sub
+
+    Private Sub ButtonNuevoDepartamento_Guardar_Click(sender As Object, e As EventArgs) Handles ButtonNuevoDepartamento_Guardar.Click
+        Dim Resp
+        Resp = MsgBox("¿Está todo correcto?", vbYesNo, "Advertencia")
+        If Resp = vbYes Then
+            Me.Close()
+        End If
+    End Sub
 End Class
